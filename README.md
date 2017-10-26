ba882f6a5d5edaf6f8ac59f9010b09967c52982f
Vitali Baranov <vitali.baranov@gmail.com>
2017-10-26 10:07:04 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Ближайшие бары
-
-[FIXME. Здесь будет описание проекта]
+# Ближайшие бары 
+Prints bars with maximum and minimum quantity of seats, 
+displays information about the bar that is closes to given
+gps coordinates.
 
 # Как запустить
 
@@ -10,9 +11,26 @@
 
 ```bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+$ python bars.py <filepath> # possibly requires call of python3 executive instead of just python
 
+Smallest bar:
+Name : БАР. СОКИ
+Address : Дубравная улица, дом 34/29
+PublicPhone : [{'PublicPhone': '(495) 258-94-19'}]
+SeatsCount : 0
+
+Biggest bar:
+Name : Спорт бар «Красная машина»
+Address : Автозаводская улица, дом 23, строение 1
+PublicPhone : [{'PublicPhone': '(905) 795-15-84'}]
+SeatsCount : 450
+
+Please input yr gps coordinates: 37.31 23.221
+The closest bar for you is: 
+Name : Бар «Ракета»
+Address : вблизи посёлка ЛМС, дом 1Б/Н
+PublicPhone : [{'PublicPhone': '(495) 676-85-55'}]
+SeatsCount : 30
 ```
 
 Запуск на Windows происходит аналогично.
--- bars.py
+++ bars.py
@@ -1,21 +1,60 @@
 import json
+import haversine
+import argparse
+
+def load_data_from_json_file(filepath):
+    with open(filepath) as json_file:
+        return json.load(json_file)
+
+def fetch_bars_from_json(json_dict):
+    return json_dict['features']
+
+def sorting_by_seats_criteria(bar):
+    return bar['properties']['Attributes']['SeatsCount']
+
+def get_biggest_bar(bars):
+    return max(bars, key=sorting_by_seats_criteria)
+
+def get_smallest_bar(bars):
+    return min(bars, key=sorting_by_seats_criteria) 
+
+def get_closest_bar(bars, longitude, latitude):
+    from_point = (longitude, latitude)
+    get_dest_point = lambda bar: bar['geometry']['coordinates']
+    closest_bar = min (
+			    	bars, 
+			    	key=lambda bar: haversine.haversine(
+                                            from_point,
+                                            get_dest_point(bar)
+			    							)
+    )
+    return closest_bar
+
+def print_bar_info(bar, additional_info=''):
+    print(additional_info)
+    for attr in ('Name', 'Address', 'PublicPhone', 'SeatsCount'):
+        print(attr, ':', bar['properties']['Attributes'][attr])
+    print()
 
 
-def load_data(filepath):
-    pass
+def get_filepath():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+    return parser.parse_args().filepath
+    
 
-
-def get_biggest_bar(data):
-    pass
-
-
-def get_smallest_bar(data):
-    pass
-
-
-def get_closest_bar(data, longitude, latitude):
-    pass
-
+def main():
+    filename = get_filepath()
+    bars_raw = load_data_from_json_file(filename)
+    bars_list = fetch_bars_from_json(bars_raw)
+    smallest_bar = get_smallest_bar(bars_list)
+    biggest_bar = get_biggest_bar(bars_list)
+    print_bar_info(smallest_bar, 'Smallest bar:')
+    print_bar_info(biggest_bar, 'Biggest bar:')
+    user_gps = (float(num) for num in 
+                    input('Please input yr gps coordinates: ').split())
+    closest = get_closest_bar(bars_list, *user_gps)
+    print_bar_info(closest, 'The closest bar for you is: ')
 
 if __name__ == '__main__':
-    pass
+    main()
